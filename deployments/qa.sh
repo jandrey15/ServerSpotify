71a9f49e0bc820b92b5e7d46245a6da5814e6244
john andrey <andrey_201015@hotmail.com>
2018-08-21 14:33:50 -0500

    Agregando secrets y env

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,6 +1,8 @@
 npm install -g now
 # sudo npm install -g --unsafe-perm now
 echo "deploying..."
+now secrets add client_id $(echo $CLIENT_ID)
+now secrets add client_secrect $(echo $CLIENT_SECRECT)
 URL=$(now --public -t $now_token)
 echo "running acceptance on $URL"
 curl --silent -L $URL
